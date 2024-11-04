f8fd095f475fe4c9b8d7d74bee536b79e252410e
Luiza Hemmel Piovesan <hemmelpiovesanl@gmail.com>
2024-11-04 19:48:19 -0300

    adicionei uma mensagem para o usuario do site

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 
 function calcularIMC(){
-  const peso = document.getElementById("peso").value;
-  const altura = document.getElementById("altura").value;
-  let imc = (peso/(altura*altura)).toFixed(1)
+  const peso = parseFloat(document.getElementById("peso").value) ;
+  const altura = parseFloat( document.getElementById("altura").value);
+  if (peso===0 || altura===0){
+    alert("o peso ou a altura não podem ser 0")
+  }else {
+    let imc = (peso/(altura*altura)).toFixed(1)
   
   
   let resultado;
   
   if ( imc < 18.5){ 
-    resultado="abaixo do peso"
+    resultado=" abaixo do peso"
   }else if ( imc >=18,5 && imc < 24.9 ){
-    resultado="peso normal"
+    resultado=" com peso normal"
   }else if ( imc >=25 && imc <29.9 ){
-    resultado="sobrepeso"
+    resultado=" com sobrepeso"
   }else if (imc >=30 && imc <34.9){
-    resultado="obesidade I"
+    resultado=" com obesidade I"
   }else if ( imc >=35 && imc <39.9){
-    resultado="obesidade II"
+    resultado=" com obesidade II"
   }else if( imc >= 40){
-    resultado="obesidade III/obesidade morbida"
+    resultado=" com obesidade III/obesidade morbida"
   }
   
-  document.getElementById("resultado").innerText = "seu imc: " + imc + "você está com"+ resultado + "."
+  document.getElementById("resultado").innerText = "seu imc: " + imc + "você está "+ resultado + "."
 
+
+  }
+
+  
 }
